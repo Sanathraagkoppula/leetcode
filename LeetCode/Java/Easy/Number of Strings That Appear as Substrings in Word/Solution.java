946de546f9c75fdd30fbb327d49b5f65a8f73130
sanath raag <sanathraagkoppula@gmail.com>
2026-06-26 15:31:45 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/Number of Strings That Appear as Substrings in Word/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        int s = patterns.length;
+
+        for (int i = 0; i < s; i++) {
+            String p = patterns[i];
+            if (word.contains(p)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
