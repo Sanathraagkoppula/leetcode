08cc5722788d7b052408bfaa6e4db05687b9a6d3
sanath raag <sanathraagkoppula@gmail.com>
2026-06-26 12:49:17 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/Find the Encrypted String/Solution.java
+++ LeetCode/Java/Easy/Find the Encrypted String/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public String getEncryptedString(String s, int k) {
-        int n = s.length();
+        s.length();
         k = k % n;
         return s.substring(k) + s.substring(0, k);
     }
