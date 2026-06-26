be9ef9c6a754ed205965534060400bad193d0b8b
sanath raag <sanathraagkoppula@gmail.com>
2026-06-26 09:24:36 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/Student Attendance Record I/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int ab=0;
+        int i;
+        char arr[]=s.toCharArray();
+        for(i=0;i<arr.length;i++){
+            char c = arr[i];
+            if(c=='A') ab++;
+            if(ab>=2){
+                return false;
+            }
+            if(s.contains("LLL")){
+                return false;
+            }
+        }
+        return true;
+    }
+}
