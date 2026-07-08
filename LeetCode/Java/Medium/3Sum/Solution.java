b947a128da5b99832f718342eb6464a474490adb
sanath raag <sanathraagkoppula@gmail.com>
2026-07-08 23:57:52 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/3Sum/Solution.java
+++ LeetCode/Java/Medium/3Sum/Solution.java
@@ -1,32 +1,38 @@
 import java.util.*;
-public class ThreeSum {
+
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums); // Step 1: Sort the array
         List<List<Integer>> result = new ArrayList<>();
+        
         for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values for i
             if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
             int left = i + 1;
             int right = nums.length - 1;
+            
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
+                
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
                     left++;
                     right--;
+                    
+                    // Skip duplicates for left and right
                     while (left < right && nums[left] == nums[left - 1]) left++;
                     while (left < right && nums[right] == nums[right + 1]) right--;
+                    
                 } else if (sum < 0) {
-                    left++;
+                    left++; // Need a larger sum
                 } else {
-                    right--;
+                    right--; // Need a smaller sum
                 }
             }
         }
+        
         return result;
-    }
-    public static void main(String[] args) {
-        ThreeSum solver = new ThreeSum();
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        System.out.println(solver.threeSum(nums));
     }
 }
